e019851ae20430433c8a805b3ef9db202c34a29a
emre gulec <emreglcfb@gmail.com>
2024-12-27 23:41:20 +0300

    remove comment line

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-const fetch = require('node-fetch'); // Fetch'i import etmelisiniz
+const fetch = require('node-fetch');
 const admin = require('firebase-admin');
 
-// Firebase Admin SDK ile kimlik doğrulama
-const serviceAccount = require('./serviceAccountKey.json');  // JSON dosyanızın yolu
+const serviceAccountBase64 = process.env.SERVICE_ACCOUNT_KEY;
+
+const serviceAccount = JSON.parse(Buffer.from(serviceAccountBase64, 'base64').toString('utf-8'));
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
-
-// Binance API'den BTC fiyatını almak (fetch ile)
 
 const fetchBtcPrice = async () => {
   try {
@@ -21,27 +20,25 @@ const fetchBtcPrice = async () => {
     const data = await response.json();
     let price = parseFloat(data.price);
 
-    // Sayıyı istediğiniz formata dönüştür
     price = price.toLocaleString('en-US', {
       minimumFractionDigits: 2,
       maximumFractionDigits: 2
     });
 
-    return price; // Formatlanmış fiyatı döndürüyoruz
+    return price;
   } catch (error) {
     console.error('Error fetching BTC price:', error);
     return null;
   }
 };
 
-// FCM token'ını kullanarak push bildirim gönderme
 const sendPushNotification = async (token, btcPrice) => {
   const message = {
     notification: {
       title: 'BTC Price Update',
       body: `Current Bitcoin price: $${btcPrice}`,
     },
-    token: token,  // Geçerli FCM token'ı
+    token: token, 
   };
 
   try {
@@ -52,9 +49,8 @@ const sendPushNotification = async (token, btcPrice) => {
   }
 };
 
-// FCM token'ı ve BTC fiyatını alıp bildirim gönderme
 const sendBtcNotification = async () => {
-  const token = 'fKiJCknhSPWZ_-OYYTUOm5:APA91bHgHkQ_5MbMRkv9UbAzNzMziDT1ARB-m-l1lzNChPnCxmn-oKOzU3FzuxUaHBrw2BnY-QUI3-xiv5DepVUP_gIBJikUIB3BV1qmvFBcaexY8NJJWy4'; // Geçerli token
+  const token = 'fKiJCknhSPWZ_-OYYTUOm5:APA91bHgHkQ_5MbMRkv9UbAzNzMziDT1ARB-m-l1lzNChPnCxmn-oKOzU3FzuxUaHBrw2BnY-QUI3-xiv5DepVUP_gIBJikUIB3BV1qmvFBcaexY8NJJWy4'; // Geçerli FCM tokeni
   const btcPrice = await fetchBtcPrice();
 
   if (btcPrice) {
@@ -64,7 +60,6 @@ const sendBtcNotification = async () => {
   }
 };
 
-// 5 dakikada bir BTC fiyatını al ve bildirim gönder
 setInterval(() => {
   sendBtcNotification();
-}, 5 * 1000); // 5 dakika = 5 * 60 * 1000 ms
+}, 5 *  1000); 
